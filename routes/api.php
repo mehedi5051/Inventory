c10824232a16730d80dddc66159a2cc5dbdbb14c
Md Mehedi Hasan <172275006+mehedi5051@users.noreply.github.com>
2026-02-12 00:38:43 +0600

    jwt_key too shotrt error fix

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\auth\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::post('/user-registration', [UserController::class, 'userRegistration']);
